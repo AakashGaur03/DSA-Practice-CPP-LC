b8461876fe0c068cab60d9312e78497024c4393c
Aakash Gaur <140057004+AakashGaur03@users.noreply.github.com>
2025-09-21 16:23:24 +0530

    Create RomantoInteger.cpp

---
+++ Day20/RomantoInteger.cpp
@@ -0,0 +1,119 @@
+// // Roman to Integer
+// // Solved
+// // Easy
+// // Topics
+// // premium lock icon
+// // Companies
+// // Hint
+// // Roman numerals are represented by seven different symbols: I, V, X, L, C,
+// D and M.
+
+// // Symbol       Value
+// // I             1
+// // V             5
+// // X             10
+// // L             50
+// // C             100
+// // D             500
+// // M             1000
+// // For example, 2 is written as II in Roman numeral, just two ones added
+// together. 12 is written as XII, which is simply X + II. The number 27 is
+// written as XXVII, which is XX + V + II.
+
+// // Roman numerals are usually written largest to smallest from left to right.
+// However, the numeral for four is not IIII. Instead, the number four is
+// written as IV. Because the one is before the five we subtract it making four.
+// The same principle applies to the number nine, which is written as IX. There
+// are six instances where subtraction is used:
+
+// // I can be placed before V (5) and X (10) to make 4 and 9.
+// // X can be placed before L (50) and C (100) to make 40 and 90.
+// // C can be placed before D (500) and M (1000) to make 400 and 900.
+// // Given a roman numeral, convert it to an integer.
+
+// // Example 1:
+
+// // Input: s = "III"
+// // Output: 3
+// // Explanation: III = 3.
+// // Example 2:
+
+// // Input: s = "LVIII"
+// // Output: 58
+// // Explanation: L = 50, V= 5, III = 3.
+// // Example 3:
+
+// // Input: s = "MCMXCIV"
+// // Output: 1994
+// // Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+// class Solution {
+// public:
+
+//     int isValue(char c){
+//         switch(c){
+//             case 'I':
+//                 return 1;
+//             case 'V':
+//                 return 5;
+//             case 'X':
+//                 return 10;
+//             case 'L':
+//                 return 50;
+//             case 'C':
+//                 return 100;
+//             case 'D':
+//                 return 500;
+//             case 'M':
+//                 return 1000;
+//         }
+//         return 0;
+//     }
+
+//     int romanToInt(string s) {
+// int res=0;
+// // Loop till second last character
+// // doesnt perform any action on last character
+//         for(int c = 0; c<s.size()-1;c++){
+//             // Means decreasing
+//             if(isValue(s[c])<isValue(s[c+1])){
+//                 res-= isValue(s[c]);
+//             }else{
+//                 res+=isValue(s[c]);
+//             }
+//         }
+//         res+=isValue(s[s.size()-1]);
+
+//         return res;
+//     }
+// };
+
+// //   int romanToInt(string s) {
+// //       unordered_map<char, int> roman = {
+// //             {'I', 1},
+// //             {'V', 5},
+// //             {'X', 10},
+// //             {'L', 50},
+// //             {'C', 100},
+// //             {'D', 500},
+// //             {'M', 1000}
+// //         };
+// //         // If in left map has increasing value then add if decreasing
+// //         // then suntract
+
+// // int res=0;
+// // // Loop till second last character
+// // // doesnt perform any action on last character
+// //         for(int c = 0; c<s.size()-1;c++){
+// //             // Means decreasing
+// //                 cout<<roman[s[c]]<<" "<<roman[s[c+1]]<<" ";
+// //             if(roman[s[c]]<roman[s[c+1]]){
+// //                 res-= roman[s[c]];
+// //             }else{
+// //                 res+=roman[s[c]];
+// //             }
+// //         }
+// //         res+=roman[s[s.size()-1]];
+
+// //         return res;
+// //     }
