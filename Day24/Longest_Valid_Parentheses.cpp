f812feaca61fd6737144123f72d845e080578eea
Aakash Gaur <140057004+AakashGaur03@users.noreply.github.com>
2025-09-25 23:10:02 +0530

    Longest Valid Parentheses

---
+++ Day24/Longest_Valid_Parentheses.cpp
@@ -0,0 +1,46 @@
+// // 32. Longest Valid Parentheses
+// // Solved
+// // Hard
+// // Topics
+// // premium lock icon
+// // Companies
+// // Given a string containing just the characters '(' and ')', return the
+// length of the longest valid (well-formed) parentheses substring.
+
+// // Example 1:
+
+// // Input: s = "(()"
+// // Output: 2
+// // Explanation: The longest valid parentheses substring is "()".
+// // Example 2:
+
+// // Input: s = ")()())"
+// // Output: 4
+// // Explanation: The longest valid parentheses substring is "()()".
+// // Example 3:
+
+// // Input: s = ""
+// // Output: 0
+// class Solution {
+// public:
+//     int longestValidParentheses(string s) {
+//         stack<int> st;
+//         st.push(-1);
+//         int ans = 0;
+//         for(int i=0;i<s.size();i++){
+//             if(s[i]=='('){
+//                 st.push(i);
+//             }else{
+//                 st.pop();
+//                 if(st.empty()){
+//                     // if stack is empty then storing most recnet invalid
+//                     st.push(i);
+//                 }else{
+//                     // Stack top contains most recent Invalid Index
+//                     ans = max(ans,i-st.top());
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
