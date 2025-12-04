9316b42e4823f650b6527f12297925e77d3224ce
Aakash Gaur <140057004+AakashGaur03@users.noreply.github.com>
2025-12-05 01:49:51 +0530

    Create Find_All_Numbers_Disappeared_in_an_Array.cpp
    With help of Solution

---
+++ Day91/Find_All_Numbers_Disappeared_in_an_Array.cpp
@@ -0,0 +1,54 @@
+// // Find All Numbers Disappeared in an Array
+// // Solved
+// // Easy
+// // Topics
+// // premium lock icon
+// // Companies
+// // Hint
+// // Given an array nums of n integers where nums[i] is in the range [1, n],
+// // return an array of all the integers in the range [1, n] that do not appear
+// in
+// // nums.
+
+// // Example 1:
+
+// // Input: nums = [4,3,2,7,8,2,3,1]
+// // Output: [5,6]
+// // Example 2:
+
+// // Input: nums = [1,1]
+// // Output: [2]
+
+// // Constraints:
+
+// // n == nums.length
+// // 1 <= n <= 105
+// // 1 <= nums[i] <= n
+
+// // Follow up: Could you do it without extra space and in O(n) runtime? You
+// may
+// // assume the returned list does not count as extra space.
+
+// class Solution {
+// public:
+//     vector<int> findDisappearedNumbers(vector<int>& nums) {
+//         // Marking Index negative of current
+//         for (int i = 0; i < nums.size(); i++) {
+//             int index = abs(nums[i]) - 1;
+//             if (nums[index] > 0) {
+//                 nums[index] = -abs(nums[index]);
+//             }
+//         }
+
+//         vector<int> res = {};
+//         // Adding non negative in result
+//         for (int i = 0; i < nums.size(); i++) {
+//             if (nums[i] > 0) {
+//                 res.push_back(i + 1);
+//             }
+//         }
+//         return res;
+//     }
+// };
+
+// WITH HELP OF SOLUTION
