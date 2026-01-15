41009e2891f75d4ee50f356f6f5c4f9ed7f28209
Aakash Gaur <140057004+AakashGaur03@users.noreply.github.com>
2026-01-15 19:42:02 +0530

    Create NextGreaterElementI.cpp

---
+++ Day133/NextGreaterElementI.cpp
@@ -0,0 +1,92 @@
+// //  Next Greater Element I
+// // Solved
+// // Easy
+// // Topics
+// // premium lock icon
+// // Companies
+// // The next greater element of some element x in an array is the first
+// greater
+// // element that is to the right of x in the same array.
+
+// // You are given two distinct 0-indexed integer arrays nums1 and nums2, where
+// // nums1 is a subset of nums2.
+
+// // For each 0 <= i < nums1.length, find the index j such that nums1[i] ==
+// // nums2[j] and determine the next greater element of nums2[j] in nums2. If
+// // there is no next greater element, then the answer for this query is -1.
+
+// // Return an array ans of length nums1.length such that ans[i] is the next
+// // greater element as described above.
+
+// // Example 1:
+
+// // Input: nums1 = [4,1,2], nums2 = [1,3,4,2]
+// // Output: [-1,3,-1]
+// // Explanation: The next greater element for each value of nums1 is as
+// follows:
+// // - 4 is underlined in nums2 = [1,3,4,2]. There is no next greater element,
+// so
+// // the answer is -1.
+// // - 1 is underlined in nums2 = [1,3,4,2]. The next greater element is 3.
+// // - 2 is underlined in nums2 = [1,3,4,2]. There is no next greater element,
+// so
+// // the answer is -1. Example 2:
+
+// // Input: nums1 = [2,4], nums2 = [1,2,3,4]
+// // Output: [3,-1]
+// // Explanation: The next greater element for each value of nums1 is as
+// follows:
+// // - 2 is underlined in nums2 = [1,2,3,4]. The next greater element is 3.
+// // - 4 is underlined in nums2 = [1,2,3,4]. There is no next greater element,
+// so
+// // the answer is -1.
+
+// class Solution {
+// public:
+//     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+//         // O(nums1.length + nums2.length)
+//         vector<int> res;
+//         stack<int> st;
+//         int n = nums2.size();
+//         map<int, int> mpp;
+//         for (int i = n - 1; i >= 0; i--) {
+//             while (!st.empty() && st.top() <= nums2[i]) {
+//                 st.pop();
+//             }
+//             if (st.empty()) {
+//                 mpp[nums2[i]] = -1;
+//                 st.push(nums2[i]);
+//             } else {
+//                 mpp[nums2[i]] = st.top();
+//                 st.push(nums2[i]);
+//             }
+//         }
+
+//         for (int i = 0; i < nums1.size(); i++) {
+//             res.push_back(mpp[nums1[i]]);
+//         }
+//         return res;
+//     }
+// };
+
+// // vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+// //     vector<int> res;
+
+// //     for (int i = 0; i < nums1.size(); i++) {
+// //         int j = 0;
+// //         while (nums2[j] != nums1[i]) {
+// //             j++;
+// //         }
+// //         j++;
+// //         int nextGreater = -1;
+// //         while (j < nums2.size()) {
+// //             if (nums2[j] > nums1[i]) {
+// //                 nextGreater = nums2[j];
+// //                 break;
+// //             }
+// //             j++;
+// //         }
+// //         res.push_back(nextGreater);
+// //     }
+// //     return res;
+// // }
