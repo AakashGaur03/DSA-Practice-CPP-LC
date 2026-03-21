e096512fa480122f4645d4176f62a4697c3d448b
Aakash Gaur <aakash.sunrise23@gmail.com>
2026-03-21 23:10:16 +0530

    Create CountCompleteTreeNodes.cpp

---
+++ Day217/CountCompleteTreeNodes.cpp
@@ -0,0 +1,81 @@
+// // Count Complete Tree Nodes
+// // Easy
+// // Topics
+// // premium lock icon
+// // Companies
+// // Given the root of a complete binary tree, return the number of the nodes
+// in
+// // the tree.
+
+// // According to Wikipedia, every level, except possibly the last, is
+// completely
+// // filled in a complete binary tree, and all nodes in the last level are as
+// far
+// // left as possible. It can have between 1 and 2h nodes inclusive at the last
+// // level h.
+
+// // Design an algorithm that runs in less than O(n) time complexity.
+
+// // Example 1:
+
+// // Input: root = [1,2,3,4,5,6]
+// // Output: 6
+// // Example 2:
+
+// // Input: root = []
+// // Output: 0
+// // Example 3:
+
+// // Input: root = [1]
+// // Output: 1
+
+// // Constraints:
+
+// // The number of nodes in the tree is in the range [0, 5 * 104].
+// // 0 <= Node.val <= 5 * 104
+// // The tree is guaranteed to be complete.
+
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+//  * right(right) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     int findLeftHeight(TreeNode* root) {
+//         int height = 0;
+//         while (root) {
+//             height++;
+//             root = root->left;
+//         }
+//         return height;
+//     }
+//     int findRightHeight(TreeNode* root) {
+//         int height = 0;
+//         while (root) {
+//             height++;
+//             root = root->right;
+//         }
+//         return height;
+//     }
+//     int countNodes(TreeNode* root) {
+//         if (root == nullptr) {
+//             return 0;
+//         }
+//         int lh = findLeftHeight(root);
+//         int rh = findRightHeight(root);
+
+//         if (lh == rh) {
+//             return (1 << lh) - 1; // this is equivalent to 2^lh-1;
+//         }
+
+//         return 1 + countNodes(root->left) + countNodes(root->right);
+//     }
+// };
