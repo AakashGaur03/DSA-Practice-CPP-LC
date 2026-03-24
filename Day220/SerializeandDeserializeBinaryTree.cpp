1bf63b8ed45ae12c1e2800bb50537e3c26962b77
Aakash Gaur <aakash.sunrise23@gmail.com>
2026-03-24 23:12:11 +0530

    SerializeandDeserializeBinaryTree.cpp

---
+++ Day220/SerializeandDeserializeBinaryTree.cpp
@@ -0,0 +1,123 @@
+// // Serialize and Deserialize Binary Tree
+// // Solved
+// // Hard
+// // Topics
+// // premium lock icon
+// // Companies
+// // Serialization is the process of converting a data structure or object into
+// a
+// // sequence of bits so that it can be stored in a file or memory buffer, or
+// // transmitted across a network connection link to be reconstructed later in
+// the
+// // same or another computer environment.
+
+// // Design an algorithm to serialize and deserialize a binary tree. There is
+// no
+// // restriction on how your serialization/deserialization algorithm should
+// work.
+// // You just need to ensure that a binary tree can be serialized to a string
+// and
+// // this string can be deserialized to the original tree structure.
+
+// // Clarification: The input/output format is the same as how LeetCode
+// serializes
+// // a binary tree. You do not necessarily need to follow this format, so
+// please
+// // be creative and come up with different approaches yourself.
+
+// // Example 1:
+
+// // Input: root = [1,2,3,null,null,4,5]
+// // Output: [1,2,3,null,null,4,5]
+// // Example 2:
+
+// // Input: root = []
+// // Output: []
+
+// // Constraints:
+
+// // The number of nodes in the tree is in the range [0, 104].
+// // -1000 <= Node.val <= 1000
+
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+//  * };
+//  */
+// class Codec {
+// public:
+//     // Encodes a tree to a single string.
+//     string serialize(TreeNode* root) {
+//         string s;
+
+//         queue<TreeNode*> q;
+//         q.push(root);
+
+//         while (!q.empty()) {
+//             TreeNode* currentNode = q.front();
+//             q.pop();
+//             if (currentNode == nullptr) {
+//                 s.append("#,");
+//             } else {
+//                 s.append(to_string(currentNode->val) + ',');
+//             }
+//             if (currentNode != nullptr) {
+//                 q.push(currentNode->left);
+//                 q.push(currentNode->right);
+//             }
+//         }
+
+//         return s;
+//     }
+
+//     // Decodes your encoded data to tree.
+//     TreeNode* deserialize(string data) {
+//         if (data.size() == 0) {
+//             return nullptr;
+//         }
+
+//         // stringstream allows the string to be iterated over as objects
+//         stringstream s(data);
+//         string str;
+
+//         // it separates the value with the help of separator in our case ','
+//         getline(s, str, ',');
+//         if (str == "#")
+//             return nullptr;
+//         TreeNode* root = new TreeNode(stoi(str));
+//         queue<TreeNode*> q;
+//         q.push(root);
+
+//         while (!q.empty()) {
+//             TreeNode* node = q.front();
+//             q.pop();
+
+//             getline(s, str, ',');
+//             if (str == "#") {
+//                 node->left = nullptr;
+//             } else {
+//                 TreeNode* leftNode = new TreeNode(stoi(str));
+//                 node->left = leftNode;
+//                 q.push(leftNode);
+//             }
+//             getline(s, str, ',');
+//             if (str == "#") {
+//                 node->right = nullptr;
+//             } else {
+//                 TreeNode* rightNode = new TreeNode(stoi(str));
+//                 node->right = rightNode;
+//                 q.push(rightNode);
+//             }
+//         }
+
+//         return root;
+//     }
+// };
+
+// // Your Codec object will be instantiated and called as such:
+// // Codec ser, deser;
+// // TreeNode* ans = deser.deserialize(ser.serialize(root));
