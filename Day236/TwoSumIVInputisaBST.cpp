c6eae76b272b04a5a6f53576649fb3e75de11cc7
Aakash Gaur <aakash.sunrise23@gmail.com>
2026-04-09 19:46:39 +0530

    Create TwoSumIVInputisaBST.cpp

---
+++ Day236/TwoSumIVInputisaBST.cpp
@@ -0,0 +1,102 @@
+// // Two Sum IV - Input is a BST
+// // Easy
+// // Topics
+// // premium lock icon
+// // Companies
+// // Given the root of a binary search tree and an integer k, return true if
+// there
+// // exist two elements in the BST such that their sum is equal to k, or false
+// // otherwise.
+
+// // Example 1:
+
+// // Input: root = [5,3,6,2,4,null,7], k = 9
+// // Output: true
+// // Example 2:
+
+// // Input: root = [5,3,6,2,4,null,7], k = 28
+// // Output: false
+
+// // Constraints:
+
+// // The number of nodes in the tree is in the range [1, 104].
+// // -104 <= Node.val <= 104
+// // root is guaranteed to be a valid binary search tree.
+// // -105 <= k <= 105
+
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+//  * right(right) {}
+//  * };
+//  */
+
+// class BSTIterator {
+
+// private:
+//     stack<TreeNode*> st;
+//     bool reverse = true;
+
+// public:
+//     BSTIterator(TreeNode* root, bool isReverse) {
+//         reverse = isReverse;
+//         pushAll(root);
+//     }
+
+//     int next() {
+//         TreeNode* temp = st.top();
+//         st.pop();
+
+//         if (reverse) {
+//             pushAll(temp->left);
+//         } else {
+//             pushAll(temp->right);
+//         }
+//         return temp->val;
+//     }
+
+// private:
+//     void pushAll(TreeNode* node) {
+//         while (node != nullptr) {
+//             st.push(node);
+//             if (reverse == true) {
+//                 node = node->right;
+//             } else {
+//                 node = node->left;
+//             }
+//         }
+//     }
+// };
+
+// class Solution {
+// public:
+//     bool findTarget(TreeNode* root, int k) {
+//         if (!root) {
+//             return false;
+//         }
+//         // For next
+//         BSTIterator l(root, false);
+//         // For before
+//         BSTIterator r(root, true);
+
+//         int i = l.next();
+//         int j = r.next();
+
+//         while (i < j) {
+//             if (i + j == k) {
+//                 return true;
+//             } else if (i + j < k) {
+//                 i = l.next();
+//             } else {
+//                 j = r.next();
+//             }
+//         }
+//         return false;
+//     }
+// };
