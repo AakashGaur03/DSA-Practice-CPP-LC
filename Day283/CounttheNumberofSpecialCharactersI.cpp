550ecfa76181475e8ff5fc310c8fd775bdb1f6de
Aakash Gaur <aakash.sunrise23@gmail.com>
2026-05-26 19:26:41 +0530

    Create CounttheNumberofSpecialCharactersI.cpp

---
+++ Day283/CounttheNumberofSpecialCharactersI.cpp
@@ -0,0 +1,62 @@
+// // Count the Number of Special Characters I
+// // Solved
+// // Easy
+// // Topics
+// // premium lock icon
+// // Companies
+// // Hint
+// // You are given a string word. A letter is called special if it appears both
+// in
+// // lowercase and uppercase in word.
+
+// // Return the number of special letters in word.
+
+// // Example 1:
+
+// // Input: word = "aaAbcBC"
+
+// // Output: 3
+
+// // Explanation:
+
+// // The special characters in word are 'a', 'b', and 'c'.
+
+// // Example 2:
+
+// // Input: word = "abc"
+
+// // Output: 0
+
+// // Explanation:
+
+// // No character in word appears in uppercase.
+
+// // Example 3:
+
+// // Input: word = "abBCab"
+
+// // Output: 1
+
+// // Explanation:
+
+// // The only special character in word is 'b'.
+
+// // Constraints:
+
+// // 1 <= word.length <= 50
+// // word consists of only lowercase and uppercase English letters.
+
+// class Solution {
+// public:
+//     int numberOfSpecialChars(string word) {
+//         set<char>s(word.begin(),word.end());
+//         int ans = 0;
+//         for(char ch = 'a';ch<='z';ch++){
+//             if(s.count(ch) && s.count(ch-'a'+'A')){
+//                 ans++;
+//             }
+//         }
+
+//         return ans;
+//     }
+// };
