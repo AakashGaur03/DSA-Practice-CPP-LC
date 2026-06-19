0486d84b0e96799255eb51210b6baf19a3cbf961
Aakash Gaur <140057004+AakashGaur03@users.noreply.github.com>
2026-06-19 22:52:28 +0530

    Create DigitFrequencyScore

---
+++ Day307/DigitFrequencyScore.cpp
@@ -0,0 +1,57 @@
+// // 3945. Digit Frequency Score
+// // Easy
+// // Topics
+// // premium lock icon
+// // Companies
+// // Hint
+// // You are given an integer n.
+
+// // The score of n is defined as the sum of d * freq(d) over all distinct digits d, where freq(d) denotes the number of times the digit d appears in n.
+
+// // Return an integer denoting the score of n.
+
+ 
+
+// // Example 1:
+
+// // Input: n = 122
+
+// // Output: 5
+
+// // Explanation:
+
+// // The digit 1 appears 1 time, contributing 1 * 1 = 1.
+// // The digit 2 appears 2 times, contributing 2 * 2 = 4.
+// // Thus, the score of n is 1 + 4 = 5.
+// // Example 2:
+
+// // Input: n = 101
+
+// // Output: 2
+
+// // Explanation:
+
+// // The digit 0 appears 1 time, contributing 0 * 1 = 0.
+// // The digit 1 appears 2 times, contributing 1 * 2 = 2.
+// // Thus, the score of n is 2.
+
+// class Solution {
+// public:
+//     int digitFrequencyScore(int n) {
+//         unordered_map<int, int> freq;
+
+//         while (n > 0) {
+//             int digit = n % 10;
+//             freq[digit]++;
+//             n /= 10;
+//         }
+
+//         int score = 0;
+
+//         for (auto it : freq) {
+//             score += it.first * it.second;
+//         }
+
+//         return score;
+//     }
+// };
