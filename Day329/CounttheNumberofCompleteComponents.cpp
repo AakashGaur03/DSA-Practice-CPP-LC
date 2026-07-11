9c5ade698c3f46780ec3539ea17016c7511c9d63
Aakash Gaur <aakash.sunrise23@gmail.com>
2026-07-11 16:41:34 +0530

    Create CounttheNumberofCompleteComponents.cpp

---
+++ Day329/CounttheNumberofCompleteComponents.cpp
@@ -0,0 +1,94 @@
+// // 2685. Count the Number of Complete Components
+// // Solved
+// // Medium
+// // Topics
+// // premium lock icon
+// // Companies
+// // Hint
+// // You are given an integer n. There is an undirected graph with n vertices,
+// // numbered from 0 to n - 1. You are given a 2D integer array edges where
+// // edges[i] = [ai, bi] denotes that there exists an undirected edge
+// connecting
+// // vertices ai and bi.
+
+// // Return the number of complete connected components of the graph.
+
+// // A connected component is a subgraph of a graph in which there exists a
+// path
+// // between any two vertices, and no vertex of the subgraph shares an edge
+// with a
+// // vertex outside of the subgraph.
+
+// // A connected component is said to be complete if there exists an edge
+// between
+// // every pair of its vertices.
+
+// // Example 1:
+
+// // Input: n = 6, edges = [[0,1],[0,2],[1,2],[3,4]]
+// // Output: 3
+// // Explanation: From the picture above, one can see that all of the
+// components
+// // of this graph are complete. Example 2:
+
+// // Input: n = 6, edges = [[0,1],[0,2],[1,2],[3,4],[3,5]]
+// // Output: 1
+// // Explanation: The component containing vertices 0, 1, and 2 is complete
+// since
+// // there is an edge between every pair of two vertices. On the other hand,
+// the
+// // component containing vertices 3, 4, and 5 is not complete since there is
+// no
+// // edge between vertices 4 and 5. Thus, the number of complete components in
+// // this graph is 1.
+
+// // Constraints:
+
+// // 1 <= n <= 50
+// // 0 <= edges.length <= n * (n - 1) / 2
+// // edges[i].length == 2
+// // 0 <= ai, bi <= n - 1
+// // ai != bi
+// // There are no repeated edges.
+
+// class Solution {
+// public:
+//     void dfs(int node, vector<int>& vis, vector<vector<int>>& adj,
+//              vector<int>& component) {
+//         vis[node] = 1;
+//         component.push_back(node);
+
+//         for (auto neigh : adj[node]) {
+//             if (!vis[neigh]) {
+//                 dfs(neigh, vis, adj, component);
+//             }
+//         }
+//     }
+//     int countCompleteComponents(int n, vector<vector<int>>& edges) {
+//         int cnt = 0;
+//         vector<int> vis(n);
+//         vector<vector<int>> adj(n);
+//         for (auto it : edges) {
+//             adj[it[0]].push_back(it[1]);
+//             adj[it[1]].push_back(it[0]);
+//         }
+//         for (int i = 0; i < n; i++) {
+//             if (vis[i])
+//                 continue;
+
+//             vector<int> component;
+
+//             dfs(i, vis, adj, component);
+//             int vertices = component.size();
+//             int degreeSum = 0;
+//             for (int node : component)
+//                 degreeSum += adj[node].size();
+
+//             int edges = degreeSum / 2;
+
+//             if (edges == vertices * (vertices - 1) / 2)
+//                 cnt++;
+//         }
+//         return cnt;
+//     }
+// };
